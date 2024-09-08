40a7913116f536d8251279bb709fe2e43649c72a
docyx <oliverxur@gmail.com>
2024-09-08 19:20:05 -0400

    fix base path in production

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
 	plugins: [vue()],
+	base: mode === 'production' ? '/spywhere/' : '/',
 	server: {
 		host: '0.0.0.0',
 	},
-})
+}))
